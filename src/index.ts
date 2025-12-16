c85c690ad819d622a0700f8937a4443ca333b815
Don Michael Feeney Jr <dfeen87@gmail.com>
2025-12-15 19:44:05 -0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+export * from "./core/errors.js";
+export * from "./core/ids.js";
+export * from "./core/audit.js";
+export * from "./core/validator.js";
+
+export * from "./identity/types.js";
+export * from "./identity/store.js";
+export * from "./identity/in_memory_store.js";
+
+export * from "./economy/types.js";
+export * from "./economy/ledger.js";
+export * from "./economy/in_memory_ledger.js";
+export * from "./economy/escrow.js";
+
+export * from "./core/transition/types.js";
+export * from "./core/transition/fsm.js";
+export * from "./core/transition/in_memory_transition_store.js";
